51fa463874f7b4cba6c2824a75d54598d76149f0
sny1411 <matteo2004@outlook.fr>
2022-11-30 22:41:59 +0100

    fin class bdd

--- src/main/java/fr/sny1411/tepakap/Main.java
+++ src/main/java/fr/sny1411/tepakap/Main.java
@@ -1,12 +1,13 @@
 package fr.sny1411.tepakap;
 
+import fr.sny1411.tepakap.sql.MysqlDb;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+    private MysqlDb bdd;
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
     }
 
     @Override
--- src/main/java/fr/sny1411/tepakap/sql/MysqlDb.java
+++ src/main/java/fr/sny1411/tepakap/sql/MysqlDb.java
@@ -1,6 +1,7 @@
 package fr.sny1411.tepakap.sql;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 import java.sql.*;
 
@@ -9,7 +10,7 @@ import java.sql.*;
  * @since 30-11-2022
  */
 public class MysqlDb {
-
+    private final Plugin plugin;
     private final String USER;
     private final String PASS;
     private final String URL;
@@ -19,57 +20,107 @@ public class MysqlDb {
     private boolean connected;
 
     /***
+     * @param plugin instance du plugin
      * @param USER nom d'utilisateur de la bdd
      * @param PASS mot de passe de la bdd
      * @param URL url de la bdd (ex : mabase.sny1411.com)
      * @param PORT port de la bdd
      * @param NAMEDB nom de la bdd
      */
-    MysqlDb(String USER,String PASS,String URL,int PORT,String NAMEDB) {
+    MysqlDb(Plugin plugin,String USER,String PASS,String URL,int PORT,String NAMEDB) {
         this.USER = USER;
         this.PASS = PASS;
         this.URL = URL;
         this.PORT = PORT;
         this.NAMEDB = NAMEDB;
+        this.plugin = plugin;
 
         connect();
     }
 
     private void connect() {
-        try {
-            long startTime = System.currentTimeMillis();
-            Connection connection = DriverManager.getConnection("jbdc:mysql://" + URL + ":" + PORT + "/" + NAMEDB, USER, PASS);
-            statement = connection.createStatement();
-            Bukkit.getServer().getConsoleSender().sendMessage("§a[INFO BDD] §2BDD CONNECTE EN " +
-                    (System.currentTimeMillis() - startTime) + " MS");
-            connected = true;
-        } catch (SQLException e) {
-            Bukkit.getServer().getConsoleSender().sendMessage("§4[ERREUR BDD] §cERREUR CONNECTION BDD ! (reload nécessaire)");
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    long startTime = System.currentTimeMillis();
+                    Connection connection = DriverManager.getConnection("jbdc:mysql://" + URL + ":" + PORT + "/" + NAMEDB, USER, PASS);
+                    statement = connection.createStatement();
+                    Bukkit.getServer().getConsoleSender().sendMessage("§a[INFO BDD] §2BDD CONNECTE EN " +
+                            (System.currentTimeMillis() - startTime) + " MS");
+                    connected = true;
+                } catch (SQLException e) {
+                    Bukkit.getServer().getConsoleSender().sendMessage("§4[ERREUR BDD] §cERREUR CONNECTION BDD ! (reload nécessaire)");
+                    while (!connected) {
+                        Bukkit.getServer().getConsoleSender().sendMessage("§4[INFO BDD] §c TENTATIVE DE RECONECTION");
+                        connect();
+                        if (connected) {
+                            Bukkit.getServer().getConsoleSender().sendMessage("§a[INFO BDD] §2TENTATIVE DE RECONNECTION EFFECTUE AVEC SUCCES");
+                        } else {
+                            Bukkit.getServer().getConsoleSender().sendMessage("§4[INFO BDD] §cTENTATIVE DE RECONNECTION ECHOUE");
+                        }
+                        try {
+                            wait(5000);
+                        } catch (InterruptedException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }
+            }
+        });
+
     }
 
+    /***
+     * @param requete sql
+     * @return ResultSet ou null si la requête échoue
+     */
     private ResultSet search(String requete) {
         try {
-            statement.executeQuery(requete);
+            if (connected) {
+                return statement.executeQuery(requete);
+            }
         } catch (SQLException e) {
             Bukkit.getServer().getConsoleSender().sendMessage("§4[ERREUR BDD] §cLA BDD N'EST PAS CONNECTE !");
             connected = false;
-            while (!connected) {
-                Bukkit.getServer().getConsoleSender().sendMessage("§4[INFO BDD] §c TENTATIVE DE RECONECTION");
-                connect();
-                if (connected) {
-                    Bukkit.getServer().getConsoleSender().sendMessage("§a[INFO BDD] §2TENTATIVE DE RECONNECTION EFFECTUE AVEC SUCCES");
-                } else {
-                    Bukkit.getServer().getConsoleSender().sendMessage("§4[INFO BDD] §cTENTATIVE DE RECONNECTION ECHOUE");
-                }
-                try {
-                    wait(5000);
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
+            connect();
         }
-
+        return null;
     }
 
+    /***
+     * @param requete sql
+     * @return ResultSet ou null si la requête échoue
+     */
+    private boolean putNewItems(String requete) {
+        try {
+            if (connected) {
+                statement.executeQuery(requete);
+                return true;
+            }
+        } catch (SQLException e) {
+            Bukkit.getServer().getConsoleSender().sendMessage("§4[ERREUR BDD] §cLA BDD N'EST PAS CONNECTE !");
+            connected = false;
+            connect();
+        }
+        return false;
+    }
+
+    /***
+     * @param requete sql
+     * @return ResultSet ou null si la requête échoue
+     */
+    private boolean modifyItems(String requete) {
+        try {
+            if (connected) {
+                statement.executeUpdate(requete);
+                return true;
+            }
+        } catch (SQLException e) {
+            Bukkit.getServer().getConsoleSender().sendMessage("§4[ERREUR BDD] §cLA BDD N'EST PAS CONNECTE !");
+            connected = false;
+            connect();
+        }
+        return false;
+    }
 }
