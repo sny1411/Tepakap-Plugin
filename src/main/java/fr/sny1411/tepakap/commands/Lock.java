334296cae781cc80a00cb149e35a7ef518d3dd2a
sny1411 <matteo2004@outlook.fr>
2022-12-04 22:52:27 +0100

    infoGui + removeFriends

--- src/main/java/fr/sny1411/tepakap/commands/Lock.java
+++ src/main/java/fr/sny1411/tepakap/commands/Lock.java
@@ -4,16 +4,24 @@ import fr.sny1411.tepakap.sql.MysqlDb;
 import fr.sny1411.tepakap.utils.secureChest.Lockable;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Objects;
+import java.util.UUID;
 
 public class Lock implements CommandExecutor {
     private static MysqlDb bdd;
@@ -32,7 +40,7 @@ public class Lock implements CommandExecutor {
             } else {
                 switch (args[0]) {
                     case "info":
-                        // open gui info
+                        infoGui(player,targeBlock);
                         break;
                     case "add":
                         if (args.length == 2) {
@@ -42,92 +50,187 @@ public class Lock implements CommandExecutor {
                         }
                         break;
                     case "remove":
-                        // delete user
+                        if (args.length == 2) {
+                            removePlayerAcces(targeBlock,player,args[1]);
+                        } else {
+                            player.sendMessage("§4[SecureChest] §cUtilise /lock remove <nomJoueur>");
+                        }
                         break;
                     case "auto":
                         // mode autolock
                 }
             }
         } else {
-            Bukkit.getServer().getConsoleSender().sendMessage("§4[CHEST] §cCommande non executable par la console");
+            Bukkit.getServer().getConsoleSender().sendMessage("§4[SecureCHEST] §cCommande non executable par la console");
         }
         return false;
     }
 
-    public static void addPlayerAcces(Block block,Player player,String addPlayer) {
-        if (Lockable.inList(block.getType())) {
-            Location locBlock = block.getLocation();
-            double X = locBlock.getX();
-            double Y = locBlock.getY();
-            double Z = locBlock.getZ();
-            String world = Objects.requireNonNull(locBlock.getWorld()).getName();
-            ResultSet result = bdd.search("SELECT UUID,id_coffre FROM COFFRE " +
-                    "WHERE coordX=" + X + " AND coordY=" + Y +
-                    " AND coordZ=" + Z + " AND monde='"+ world + "'");
+    public static void infoGui(Player player, Block block) {
+        Location locBlock = block.getLocation();
+        double X = locBlock.getX();
+        double Y = locBlock.getY();
+        double Z = locBlock.getZ();
+        String world = Objects.requireNonNull(locBlock.getWorld()).getName();
+        ResultSet result = searchCoffre(block,X,Y,Z,world, player);
+        try {
+            if (result.next()) {
+                String UUIDowner = result.getString("UUID");
+                int id_coffre = result.getInt("id_coffre");
+                Inventory infoGui = Bukkit.createInventory(null, InventoryType.CHEST, "info");
+                ItemStack itemGlass = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
+                ItemMeta metaGlass = itemGlass.getItemMeta();
+                metaGlass.setDisplayName(" ");
+                itemGlass.setItemMeta(metaGlass);
 
-            try {
-                if (result.next()) {
-                    int idCoffre = result.getInt("id_coffre");
-                    if (result.getString("UUID").equals(player.getUniqueId().toString())) {
-                        ResultSet searchPlayerAdd = bdd.search("SELECT UUID FROM JOUEUR WHERE pseudo='" + addPlayer + "'");
-                        if (searchPlayerAdd.next()) {
-                            String UUIDFriend = searchPlayerAdd.getString("UUID");
-                            ResultSet resultInBdd = bdd.search("SELECT * FROM ACCEDE WHERE UUID='" + UUIDFriend + "'");
-                            if (!resultInBdd.next()) {
-                                ResultSet resultNbreFriends = bdd.search("SELECT COUNT(*) AS 'nbre_friends' FROM ACCEDE WHERE id_coffre=" + idCoffre);
-                                resultNbreFriends.next();
-                                int nbreFriends = resultNbreFriends.getInt("nbre_friends");
-                                if (nbreFriends < 7) {
-                                    bdd.putNewItems("INSERT INTO ACCEDE VALUES ('" + UUIDFriend + "'," + idCoffre + ")");
-                                    player.sendMessage("§2[SecureChest] §aVotre ami(e) à était ajouté(e)");
-                                } else {
-                                    player.sendMessage("§4[SecureChest] §cVous avez atteins la limite de personne ayant accès à votre block");
-                                }
+                ResultSet resultListFriends = bdd.search("SELECT UUID FROM ACCEDE WHERE id_coffre=" + id_coffre);
 
-                            } else {
-                                player.sendMessage("§4[SecureChest] §cCette personne à déjà accès à ce block");
+                for (int i = 0; i < 27; i++) {
+                    switch (i) {
+                        case 4:
+                            OfflinePlayer Owner = Bukkit.getOfflinePlayer(UUID.fromString(UUIDowner));
+                            ItemStack OwnerHead = new ItemStack(Material.PLAYER_HEAD);
+                            SkullMeta skullMetaOwner = (SkullMeta)OwnerHead.getItemMeta();
+                            skullMetaOwner.setOwningPlayer(Owner);
+                            OwnerHead.setItemMeta(skullMetaOwner);
+                            infoGui.setItem(i, OwnerHead);
+                            break;
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                            if (resultListFriends.next()) {
+                                String UUIDFriend = resultListFriends.getString("UUID");
+                                OfflinePlayer friend = Bukkit.getOfflinePlayer(UUID.fromString(UUIDFriend));
+                                ItemStack friendHead = new ItemStack(Material.PLAYER_HEAD);
+                                SkullMeta skullMeta = (SkullMeta)friendHead.getItemMeta();
+                                skullMeta.setOwningPlayer(friend);
+                                friendHead.setItemMeta(skullMeta);
+                                infoGui.setItem(i, friendHead);
                             }
-
-                        } else {
-                            player.sendMessage("§4[SecureChest] §cVotre ami(e) n'a pas était trouvé :'(");
-                        }
-                    } else {
-                        player.sendMessage("§4[SecureChest] §cCe block ne vous appartient pas");
+                            break;
+                        default:
+                            infoGui.setItem(i, itemGlass);
+                            break;
                     }
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
+                player.openInventory(infoGui);
+            } else {
+                player.sendMessage("§4[SecureChest] §cBlock non protégé");
             }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+
+
+    public static void removePlayerAcces(Block block,Player player,String addPlayer) {
+        Location locBlock = block.getLocation();
+        double X = locBlock.getX();
+        double Y = locBlock.getY();
+        double Z = locBlock.getZ();
+        String world = Objects.requireNonNull(locBlock.getWorld()).getName();
+        ResultSet result = searchCoffre(block,X,Y,Z,world, player);
+        try {
+            if (result.next()) {
+                int idCoffre = result.getInt("id_coffre");
+                String UUIDProprio = result.getString("UUID");
+                if (UUIDProprio.equals(player.getUniqueId())) {
+                    ResultSet searchPlayerAdd = bdd.search("SELECT UUID FROM JOUEUR WHERE pseudo='" + addPlayer + "'");
+                    if (searchPlayerAdd.next()) {
+                        String UUIDFriend = searchPlayerAdd.getString("UUID");
+                        ResultSet resultInBdd = bdd.search("SELECT * FROM ACCEDE WHERE UUID='" + UUIDFriend + "'");
+                        if (resultInBdd.next()) {
+                            bdd.modifyItems("DELETE FROM ACCEDE WHERE UUID='" + UUIDFriend + "' AND id_coffre=" + idCoffre);
+                            player.sendMessage("§2[SecureChest] §aVotre ami(e) n'est plus votre ami(e) (pas sympa ça)");
+                        } else {
+                            player.sendMessage("§4[SecureChest] §cCette personne n'avais pas accès à ce block");
+                        }
+                    } else {
+                        player.sendMessage("§4[SecureChest] §cVotre ami(e) n'a pas était trouvé :'(");
+                    }
+                } else {
+                    player.setDisplayName("§4[SecureChest] §cVous n'êtes pas le propriétaire");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addPlayerAcces(Block block,Player player,String addPlayer) {
+        Location locBlock = block.getLocation();
+        double X = locBlock.getX();
+        double Y = locBlock.getY();
+        double Z = locBlock.getZ();
+        String world = Objects.requireNonNull(locBlock.getWorld()).getName();
+        ResultSet result = searchCoffre(block,X,Y,Z,world, player);
+        try {
+            if (result.next()) {
+                int idCoffre = result.getInt("id_coffre");
+                if (result.getString("UUID").equals(player.getUniqueId().toString())) {
+                    ResultSet searchPlayerAdd = bdd.search("SELECT UUID FROM JOUEUR WHERE pseudo='" + addPlayer + "'");
+                    if (searchPlayerAdd.next()) {
+                        String UUIDFriend = searchPlayerAdd.getString("UUID");
+                        ResultSet resultInBdd = bdd.search("SELECT * FROM ACCEDE WHERE UUID='" + UUIDFriend + "'");
+                        if (!resultInBdd.next()) {
+                            ResultSet resultNbreFriends = bdd.search("SELECT COUNT(*) AS 'nbre_friends' FROM ACCEDE WHERE id_coffre=" + idCoffre);
+                            resultNbreFriends.next();
+                            int nbreFriends = resultNbreFriends.getInt("nbre_friends");
+                            if (nbreFriends < 7) {
+                                bdd.putNewItems("INSERT INTO ACCEDE VALUES ('" + UUIDFriend + "'," + idCoffre + ")");
+                                player.sendMessage("§2[SecureChest] §aVotre ami(e) à était ajouté(e)");
+                            } else {
+                                player.sendMessage("§4[SecureChest] §cVous avez atteins la limite de personne ayant accès à votre block");
+                            }
+                        } else {
+                            player.sendMessage("§4[SecureChest] §cCette personne à déjà accès à ce block");
+                        }
+
+                    } else {
+                        player.sendMessage("§4[SecureChest] §cVotre ami(e) n'a pas était trouvé :'(");
+                    }
+                } else {
+                    player.sendMessage("§4[SecureChest] §cCe block ne vous appartient pas");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
     public static void lock(Block block,Player player) {
-        if (Lockable.inList(block.getType())) {
-            Location locBlock = block.getLocation();
-            double X = locBlock.getX();
-            double Y = locBlock.getY();
-            double Z = locBlock.getZ();
-            String world = Objects.requireNonNull(locBlock.getWorld()).getName();
-            ResultSet result = bdd.search("SELECT COUNT(id_coffre) AS 'nb_coffre' FROM COFFRE " +
-                    "WHERE coordX=" + X + " AND coordY=" + Y +
-                    " AND coordZ=" + Z + " AND monde='"+ world + "'");
-
-            int nb_coffre = -1;
-            try {
-                result.next();
-                nb_coffre = result.getInt("nb_coffre");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            if (nb_coffre == 0) {
+        Location locBlock = block.getLocation();
+        double X = locBlock.getX();
+        double Y = locBlock.getY();
+        double Z = locBlock.getZ();
+        String world = Objects.requireNonNull(locBlock.getWorld()).getName();
+        ResultSet result = searchCoffre(block,X,Y,Z,world, player);
+        try {
+            if (!result.next()) {
                 bdd.putNewItems("INSERT INTO COFFRE(coordX,coordY,coordZ,monde,UUID) VALUES(" + X + "," + Y+","+Z + ",'" + world + "','" + player.getUniqueId() + "')");
                 player.sendMessage("§2[SecureChest] §aCoffre sécurisé");
             } else {
                 player.sendMessage("§4 [SecureChest] §cCe coffre est déjà sécurisé");
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ResultSet searchCoffre(Block block,double X,double Y,double Z,String world,Player player) {
+        if (Lockable.inList(block.getType())) {
+            ResultSet result = bdd.search("SELECT UUID,id_coffre FROM COFFRE " +
+                    "WHERE coordX=" + X + " AND coordY=" + Y +
+                    " AND coordZ=" + Z + " AND monde='" + world + "'");
+            return result;
         } else {
             player.sendMessage("§4[SecureChest] §cCe block n'est pas protégeable");
         }
+        return null;
     }
 }
